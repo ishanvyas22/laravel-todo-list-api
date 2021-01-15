723b65b58968c173142baf86fbbfe42ea94da977
Ishan Vyas <isvyas@gmail.com>
2021-01-15 15:28:39 +0530

    Add task factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'parent_id' => null,
+            'title' => $this->faker->text(),
+            'due_date' => now()->add(1, 'day')->format('Y-m-d'),
+            'status' => false,
+            'deleted_at' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
